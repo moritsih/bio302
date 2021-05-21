221a2128f24091f96b0302a11ddd41c338427920
moritsih <Moritz.Haderer411@gmail.com>
2021-05-21 11:36:59 +0200

    Files created on the first day of bio302

---
+++ Lecture1.R
@@ -0,0 +1,22 @@
+
+
+devtools::install_github("biostats-r/biostats.tutorials")
+library(biostats.tutorials)
+### install.packages("lme4") ###
+learnr::run_tutorial("Dates-and-times", package = "biostats.tutorials")
+
+# naming-objects - WORKING
+# function-packages - DONE
+# importing-data - NOT AVAILABLE
+# using-dplyr - NOT FINISHED
+# Dates-and-times - DONE
+# text-manipulation - WORKING
+
+# packages available:
+# - Dates-and-times   : "Working with dates and ti...
+# - function-packages : "Functions and Packages"
+# - naming-objects    : "Naming objects"
+# - text-manipulation : "Processing text with `str...
+# - tidying-data      : "Tidying data with tidyr"
+# - using-dplyr       : "Using dplyr"
+
