74e4accca9ac095abf8c5c2f20c30c715e15b7e1
Moritz <71453055+moritsih@users.noreply.github.com>
2021-05-22 11:49:02 +0200

    Update Mt_Gonga_analysis.R

--- Mt_Gonga_analysis.R
+++ Mt_Gonga_analysis.R
@@ -1,33 +1,44 @@
 
+
 # importing data
 library(readxl)
 library(tidyverse)
 library(ggplot2)
-biomass2015 <- read_excel("biomass2015.xls")
-View(biomass2015)
+
+biomass2015 <- read_excel("biomass2015.xls", sheet = 1)
+
+for (i in 2:4) {
+  biomass2015 <- rbind(biomass2015, read_excel("biomass2015.xls", sheet = i))
+}
+
+view(biomass2015)
 
 
 # excluding irrelevant columns
 biomass_clean <- biomass2015 %>%
-
+  
   #selecting only relevant columns
-  select(plot, production) %>%
-
-  #excluding NA values
+  select(c(site, plot, production)) %>%
+  
+  dplyr::mutate(site = factor(site,
+                              levels = c("L", "M", "A", "H"))) %>%
+    #excluding NA values
   filter(!is.na(production)) %>%
-
-  #grouping by plot
-  group_by(plot) %>%
-
+  
+  #grouping by site AND plot
+  group_by(site, plot) %>%
+  
   #getting sum by plot
-  summarise(biomass_sum = sum(production))
+  summarise(Total_biomass = sum(production))
+
+
 
 View(biomass_clean)
 
-##Anne was here ##
 
-biomass_barplot <- ggplot(biomass_clean,
-                          aes(x = plot,
-                              y = biomass_sum)) + geom_bar(stat = "identity")
+biomass_boxplot <- ggplot(biomass_clean,
+                          aes(x = site, 
+                              y = Total_biomass)) + geom_boxplot()
 
-print(biomass_barplot)
+print(biomass_boxplot)
+
